4c121ffeb70ab6c500891cacbd9185f641669476
msyanzi <maxyay5566@gmail.com>
2014-03-23 22:40:04 +0800

    update profile.css&html

---
+++ profile.js
@@ -0,0 +1,34 @@
+// Profil card 
+//----------------
+// A design collaboration with @auareyou
+// design : http://dribbble.com/shots/1416761-Profile-bio
+// 2014 @LukyVj
+// http://lucasbonomi.com
+
+//Nano jquery template engine 
+// https://github.com/trix/nano
+/*
+ data = {
+            user: {
+                first_name: "Lucas",
+                last_name: "Bonomi",
+                desc_one:"UI UX Designer \n",
+                desc_two:"Rocket Design co-founder",
+                links: {
+                  website: "http://lucasbonomi.com",
+                  twitter: "http://twitter.com/LukyVj",
+                  dribbble:"http://dribbble.com/LukyVj"
+                }
+            }
+        }
+ 
+ 
+ window.onload = function () {
+        document.getElementById('name').innerHTML = nano("{user.first_name} {user.last_name}", data);
+    document.getElementById('desc_one').innerHTML = nano("{user.desc_one} ", data);
+   document.getElementById('desc_two').innerHTML = nano(" {user.desc_two}", data); 
+   document.getElementById('website').attr('href') = nano(" {user.links.website}", data);
+      document.getElementById('twitter').attr('href')= nano(" {user.links.twitter}", data);
+      document.getElementById('dribbble').attr('href') = nano(" {user.links.dribbble}", data);  
+    }   */
+
